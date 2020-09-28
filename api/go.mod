46ad83f7b6f43181973cff372534d66ecbf4d48f
1k-ct <268iop@gmail.com>
2020-09-29 01:29:05 +0900

    up

---
+++ api/go.mod
@@ -0,0 +1,5 @@
+module github.com/1k-ct/circleci_test
+
+go 1.13
+
+require github.com/labstack/echo/v4 v4.1.17
--- sample01.go
+++
@@ -1,6 +0,0 @@
-package sample01
-
-//HelloWorld return => "hello world, " + s(string)
-func HelloWorld(s string) string {
-	return "hello world, " + s
-}
